c30e8a6601976d475429cf51b3ef35cd064cce04
Administrator <1476104805@qq.com>
2020-10-25 12:24:18 +0800

    添加：1.创建页面：学习笔记主页（映射URL，编辑视图，编写模板）      2.创建其他页面（模板继承，显示所有主题的页面，显示特定主题的页面）

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # 主页
     path('', views.index, name='index'),
+    # 显示所有主题
+    path('topics/', views.topics, name='topics'),
+    # 特定主题的详情页
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
+
 
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """显示所有的主题"""
+    topics_data = Topic.objects.order_by('date_added')
+    context = {'topics': topics_data}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """显示单个主题及其所有的条目"""
+    topic_data = Topic.objects.get(id=topic_id)
+    entries = topic_data.entry_set.order_by('-date_added')
+    context = {'topic': topic_data, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
