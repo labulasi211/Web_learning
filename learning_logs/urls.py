0d9aa37a3d521ea6c91cc707c3d4fea3ffc5c05d
Zeng <1476104805@qq.com>
2020-10-29 17:05:41 +0800

    添加：添加新主题；添加新条目

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     # 显示所有主题
     path('topics/', views.topics, name='topics'),
     # 特定主题的详情页
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # 用于添加新主题的页面
+    path('new_topic/', views.new_topic, name='new topic'),
+    # 用于添加新条目的页面
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from  .forms import TopicForm,EntryForm
 
 # Create your views here.
 
@@ -23,3 +24,41 @@ def topic(request, topic_id):
     entries = topic_data.entry_set.order_by('-date_added')
     context = {'topic': topic_data, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # 显示空表格或指出表单数据无效
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """在特定主题中添加新条目"""
+    topic_data = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据：创建一个空表单
+        form = EntryForm()
+    else:
+        # POST 提交的数据：对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry_data = form.save(commit=False)
+            new_entry_data.topic = topic_data
+            new_entry_data.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # 显示空表单或指出表单数据无效
+    context = {'form': form, 'topic': topic_data}
+    return render(request, 'learning_logs/new_entry.html', context)
